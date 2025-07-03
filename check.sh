44605414fc8c37dab37633869242175cac528da2
galaxy-cli <kvnallbright@gmail.com>
2025-07-03 05:02:42 -0400

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-##############
-#            #
-#  check.sh  #
-#            #
-##############
+#   ___| |__   ___  ___| | __  ___| |__
+#  / __| '_ \ / _ \/ __| |/ / / __| '_ \
+# | (__| | | |  __/ (__|   < _\__ \ | | |
+#  \___|_| |_|\___|\___|_|\_(_)___/_| |_|
+# https://github.com/galaxy-cli/check
+# check.sh - Spellcheck helper tool
 
+# `trap` to store temp files
 temp=$(mktemp)
 
+# User input prompt
 read -r input
 
+# Stores user input as temp file
 echo "$input" > "$temp"
 
+# User input is shell checked
 aspell -c "$temp"
 
-cat "$temp"'
+# Prints corrected user input
+cat "$temp"
